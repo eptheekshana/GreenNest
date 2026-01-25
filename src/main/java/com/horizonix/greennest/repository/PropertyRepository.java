0e6d845d1a16f36a9a121741aceb32092cecc115
E P Theekshana <eptheekshana@gmail.com>
2026-01-25 11:00:58 +0530

    PropertyRepository.java Updated

--- src/main/java/com/horizonix/greennest/repository/PropertyRepository.java
+++ src/main/java/com/horizonix/greennest/repository/PropertyRepository.java
@@ -2,7 +2,7 @@ package com.horizonix.greennest.repository;
 
 import com.horizonix.greennest.entity.Property;
 import com.horizonix.greennest.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;98888*547.
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
