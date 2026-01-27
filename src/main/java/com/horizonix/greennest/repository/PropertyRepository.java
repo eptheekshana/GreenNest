cbd9cd71e95acc6b0d17d74e47297b2da73a1fea
Sachini Senarathna <sachinisenarathnans1007@gmail.com>
2026-01-27 22:13:14 +0530

    Added PropertyRepository for search logic

--- src/main/java/com/horizonix/greennest/repository/PropertyRepository.java
+++ src/main/java/com/horizonix/greennest/repository/PropertyRepository.java
@@ -1,16 +1,13 @@
 package com.horizonix.greennest.repository;
 
 import com.horizonix.greennest.entity.Property;
-import com.horizonix.greennest.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface PropertyRepository extends JpaRepository<Property, Long> {
 
-    // Find all properties created by a specific owner
-    List<Property> findByOwner(User owner);
+    List<Property> findByLocationContainingIgnoreCaseAndPriceLessThanEqual(String location, Double price);
 
-    // Search properties by location
-    List<Property> findByLocationContainingIgnoreCase(String location);
 }
